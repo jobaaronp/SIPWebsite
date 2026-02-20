62a5a9fa8e48e4bde00f467ee60c6a9a5add32c9
Job Aaron T. Pimentel <jobaaron444@gmail.com>
2026-02-19 19:18:20 -0600

    fix: update BrowserRouter basename to include full URL

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter basename="/SIPWebsite">
+  <BrowserRouter basename="/https://jobaaronp.github.io/SIPWebsite/">
     <App />
   </BrowserRouter>
 )
